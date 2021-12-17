91e01979ad22e757791c8195343344554f7b4807
lizhuoli <lizhuoli@bytedance.com>
2021-12-17 15:06:39 +0800

    Fix the minumum version of iOS/tvOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SDWebImageSVGKitPlugin",
     platforms: [
-        .macOS(.v10_11), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
